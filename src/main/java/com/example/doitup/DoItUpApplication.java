8f37db0d8bc2f57fb20b9760d7b44f56445a6030
DONGBISULL <sdb7734@gmail.comgit config --global user.name sulldongbi>
2023-06-01 23:14:54 +0900

    Init

---
+++ src/main/java/com/example/doitup/DoItUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.doitup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoItUpApplication.class, args);
+	}
+
+}
